76f22c06036bd1eba70b867add7b3f270b1f1f25
Vitor Souza <vhsouzaeng@gmail.com>
2021-01-26 10:28:43 -0300

    feat: activing modal

--- src/views/Store/index.tsx
+++ src/views/Store/index.tsx
@@ -1,34 +1,40 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { StoreContext } from "../../contexts/StoreContext";
-import Cartbutton from "../../shared/components/CartButton";
-import CartList from "../../shared/components/CartList";
+import { useToggle } from "../../hooks/useToogle";
+import Cartbutton from "./components/CartButton";
+import CartList from "./components/CartList";
+import ModalConfirm from "../../shared/components/ModalConfirm";
 import Card from "./components/Card";
 import { Container, Content, GridList, HeaderInput } from "./store.style";
 
 const Store: React.FC = () => {
 
     const { pokemons, loadStore } = useContext(StoreContext);
+    const { active, enabled, disabled } = useToggle(false);
 
     useEffect(() => {
         loadStore()
     }, []);
 
     return (
-        <Container>
-            <header>
-                <img src="https://raw.githubusercontent.com/PokeMiners/pogo_assets/master/Images/Types/POKEMON_TYPE_FIRE.png" alt="logo" />
-                <HeaderInput />
-                <div>
-                    <Cartbutton />
-                </div>
-            </header>
-            <Content>
-                <GridList>
-                    {pokemons.length > 1 && pokemons.map(p => <Card key={p.name} img={p.img} name={p.name} value={p.value} />)}
-                </GridList>
-                <CartList />
-            </Content>
-        </Container>
+        <>
+            <Container>
+                <header>
+                    <img src="https://raw.githubusercontent.com/PokeMiners/pogo_assets/master/Images/Types/POKEMON_TYPE_FIRE.png" alt="logo" />
+                    <HeaderInput />
+                    <div>
+                        <Cartbutton />
+                    </div>
+                </header>
+                <Content>
+                    <GridList>
+                        {pokemons.length > 1 && pokemons.map(p => <Card key={p.name} img={p.img} name={p.name} value={p.value} />)}
+                    </GridList>
+                    <CartList setIsOpen={enabled} />
+                </Content>
+            </Container>
+            {active && <ModalConfirm isOpen={active} setIsOpen={disabled} />}
+        </>
     );
 };
 
